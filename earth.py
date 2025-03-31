b1f664abb7d8d7046937673fb642a744c51f5da2
BautistaPerezF <162722097+BautistaPerezF@users.noreply.github.com>
2025-03-31 11:55:47 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -1,3 +1,9 @@
 def earth():
     x = "Bangladesh"
     y = "Barbados"
+
+    if x < y:
+        print (f"The result of {x} come first in the dictionary than {y} is {True}")
+    if x < y:
+        print (f"The result of {y} come first in the dictionary than {x} is {False}")
+
