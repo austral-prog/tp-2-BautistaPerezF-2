007e6f5adbe94aacde40512a1eb6ef7824160bf8
BautistaPerezF <162722097+BautistaPerezF@users.noreply.github.com>
2025-03-31 12:02:24 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,3 +1,9 @@
-def change():
+def change ():
     expense = 23.75
     money = 100
+    vuelto = money - expense
+    pesos = int(vuelto)
+    centavos = vuelto - pesos
+    resultado_centavo = int(centavos*100)
+    print (f"pesos:{pesos}")
+    print (f"centavos:{resultado_centavo}")
